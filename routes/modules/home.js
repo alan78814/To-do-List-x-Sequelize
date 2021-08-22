534ed45fbc3ed6364e4a27f7ef485ac237ce27f9
alan78814 <tri78814@gmail.com>
2021-08-22 14:55:26 +0800

    feat: add relations between User and Todo models

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -3,13 +3,20 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+const User = db.User
 
 // 定義首頁路由
 router.get('/', (req, res) => {
-    return Todo.findAll({
-        raw: true,
-        nest: true
-    })
+    User.findByPk(req.user.id)
+        .then((user) => {
+            if (!user) throw new Error('user not found')
+
+            return Todo.findAll({
+                raw: true,
+                nest: true,
+                where: { UserId: req.user.id }
+            })
+        })
         .then(todos => res.render('index', { todos }))
         .catch(error => console.error(error))
 })
