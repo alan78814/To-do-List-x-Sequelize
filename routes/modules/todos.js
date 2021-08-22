ad6094138cc7ec6d2daddca7af51b639b298bb41
alan78814 <tri78814@gmail.com>
2021-08-22 15:22:25 +0800

    feat: add update todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -27,4 +27,33 @@ router.get('/:id', (req, res) => {
         .catch(error => console.log(error))
 })
 
+router.get('/:id/edit', (req, res) => {
+    const UserId = req.user.id
+    const id = req.params.id
+
+    return Todo.findOne({ where: { id, UserId } })
+        .then(todo => {
+            // console.log(todo.toJSON(), todo.get())
+            // https://medium.com/tds-note/get-pure-object-from-sequelize-instance-8be199610ae5
+            res.render('edit', { todo: todo.get() })
+        })
+        .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+    const UserId = req.user.id
+    const id = req.params.id
+    const { name, isDone } = req.body
+    console.log(req.body)
+
+    return Todo.findOne({ where: { id, UserId } })
+        .then(todo => {
+            todo.name = name
+            todo.isDone = isDone === 'on'
+            return todo.save()
+        })
+        .then(() => res.redirect(`/todos/${id}`))
+        .catch(error => console.log(error))
+})
+
 module.exports = router
