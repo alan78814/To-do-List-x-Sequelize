7655e6962c1b43f394d372d4ea10a82e9ae251ab
alan78814 <tri78814@gmail.com>
2021-08-22 14:35:04 +0800

    feat: add isAuthenticated to display different view templates

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require('express-session')
 const usePassport = require('./config/passport')
 const routes = require('./routes')
 
+
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -18,7 +19,14 @@ app.use(session({
     saveUninitialized: true
 }))
 
+// 呼叫 Passport 函式並傳入 app，這條要寫在路由之前
 usePassport(app)
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.isAuthenticated()
+    res.locals.user = req.user
+    next()
+})
 
 app.use(routes)
 
